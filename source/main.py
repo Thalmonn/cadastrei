cf3be056d53f740539bda513b3c1042839baf368
Thalmonn <thalmonnsales@gmail.com>
2021-02-03 18:09:08 -0300

    Configurando primeiras definições de janela.

--- source/main.py
+++ source/main.py
@@ -1,1 +1,19 @@
-print('Teste')
+# Importando bibliotecas
+
+import tkinter as tk
+from modules import janelaconfig
+
+# Criando janela de menu principal.
+
+root = tk.Tk()
+menu_principal = root
+
+# Configuração dimensoes e centraliza a janela principal.
+
+menu_principal.geometry(f'800x600')
+janelaconfig.centraliza_janela(menu_principal)
+
+# Inicializa aplicação.
+
+if __name__ == '__main__':
+    menu_principal.mainloop()
