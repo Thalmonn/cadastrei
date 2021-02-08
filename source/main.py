21fc583e679994c239a0321bf77d76118b9aa066
Thalmonn <thalmonnsales@gmail.com>
2021-02-08 14:00:48 -0300

    Alterações em forma de importação de modulos.

--- source/main.py
+++ source/main.py
@@ -1,23 +1,13 @@
 # Importação das bibliotecas e arquivos da aplicação.
 
-import os
 import tkinter as tk
-from modules import janelaconfig
+from modules import *
 
-# Função que importa todos os assets para aplicação.
-
-
-def asset(item):
-
-    root_main = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(root_main, 'resources', *item.split('/'))
-
-
-# Cria e configura janela principal.
+# Configura GUI.
 
 janela_principal = tk.Tk()  # Cria objeto janela principal.
 janela_principal.title('Cadastrei')  # Define o título da janela.
-janela_principal.iconbitmap(asset('img/icons/mainicon.ico'))  # Define o ícone da janela principal.
+janela_principal.iconbitmap('resources/img/icons/mainicon.ico')  # Define o ícone da janela principal.
 config_janelaprincipal = janelaconfig.Janela(master=janela_principal)  # Acessa configuração janelaconfig.
 config_janelaprincipal.centraliza_tamanho(janela=janela_principal)  # Centraliza a janela na tela.
 
