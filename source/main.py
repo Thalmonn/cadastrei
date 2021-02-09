a8c790570f2732694a56cf5e9d66080d443270aa
Thalmonn <thalmonnsales@gmail.com>
2021-02-09 09:59:34 -0300

    Alterado formatação do menu inicial.

--- source/main.py
+++ source/main.py
@@ -17,7 +17,10 @@ janela_principal['bg'] = '#185c37'
 # Logo Principal
 
 logo_principal = tk.PhotoImage(file='resources/img/logos/logomain.png')  # Define arquivo de logo.
-label_logoprincipal = tk.Label(master=janela_principal, image=logo_principal, bg='#185c37').grid()  # Posiciona logo.
+label_logoprincipal = tk.Label(master=janela_principal, image=logo_principal,
+                               bg='#185c37').grid(row=0, column=0, sticky='n')  # Posiciona logo.
+janela_principal.grid_rowconfigure(0, weight=1)
+janela_principal.grid_columnconfigure(0, weight=1)
 
 # FRAMES
 
@@ -27,7 +30,7 @@ frame_menu = menu.MenuPrincipal(master=janela_principal)  # Insere menu na janel
 
 # Configura Layout.
 
-frame_menu.grid()  # Chama o frame do menu principal.
+frame_menu.grid(sticky='', padx=120, pady=120)  # Carrega o frame do menu principal.
 
 # Inicia aplicação.
 
--- source/modules/menu.py
+++ source/modules/menu.py
@@ -24,14 +24,33 @@ class MenuPrincipal(tk.Frame):
 
         ver_clientes = tk.Button(self,
                                  text='Visualizar',
-                                 bg='#33c481',
+                                 bg='#47c9a8',
                                  font='Roboto 12 bold',
                                  width=10,
                                  height=2,
                                  bd=5,
                                  relief='raised'
                                  )
+
+        sair_sistema = tk.Button(self,
+                                 text='Sair',
+                                 bg='#de2701',
+                                 font='Roboto 12 bold',
+                                 width=10,
+                                 height=2,
+                                 bd=5,
+                                 relief='raised'
+                                 )
+
     # Layout
 
-        cadastro_cliente.grid(row=0, column=0)
-        ver_clientes.grid(row=1, column=0)
+        cadastro_cliente.grid(row=1, column=0, sticky='n')
+        ver_clientes.grid(row=2, column=0, sticky='')
+        sair_sistema.grid(row=3, column=0, sticky='')
+
+        # cadastro_cliente.grid_rowconfigure(1, weight=1)
+        # cadastro_cliente.grid_columnconfigure(0, weight=1)
+        # ver_clientes.grid_rowconfigure(2, weight=1)
+        # ver_clientes.grid_columnconfigure(0, weight=1)
+        # sair_sistema.grid_rowconfigure(3, weight=1)
+        # sair_sistema.grid_columnconfigure(0, weight=1)
